cd7c3f0386f397086c10eeacd70a1c16268013a2
Estevam <92314399+Estevam2302@users.noreply.github.com>
2021-11-16 12:52:56 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,41 +1,42 @@
-print('Adicionar à fila preferencial: P')
-print('Atender alguém da fila preferencial: L')
-print('Adicionar à fila normal: F')
-print('Atender alguém da fila normal: A')
-print('Para sair: S')
-print('Para rever os comandos: comandos')
+print('Para adicionar à fila preferencial digite: P')
+print('Para atender alguém da fila preferencial digite: L')
+print('Para adicionar à fila normal digite: F')
+print('Para atender alguém da fila normal digite: A')
+print('para sair digite: S')
+print('Para rever os comandos digite C')
+fila_p=list()
+fila_n=list()
 últimoP=0
 últimoN=0
-fila_preferencial=list()
-fila_normal=list()
+s=False
 while True:
-    if len(fila_preferencial + fila_normal) == 0:
-        print('Filas vazias, ninguem para atender!')
-    elif len(fila_preferencial + fila_normal) > 0:
-        print('Fila prefrencial: ', len(fila_preferencial))
-        print('Fila normal: ', len(fila_normal))
-    comando = input('Insira um comando: ')
-    if comando == 'P':
-        últimoP+=1
-        fila_preferencial.append(últimoP)
-    elif comando == 'L':
-        atendido = fila_preferencial.pop(0)
-        print('Cliente P %d atendido' %atendido)
-    elif comando == 'F':
-        últimoN+=1
-        fila_normal.append(últimoN)
-    elif comando == 'A':
-        atendido = fila_normal.pop(0)
-        print('Cliente N %d atendido' %atendido)  
-    elif comando == 'comandos':
-        print('Adicionar à fila preferencial: P')
-        print('Atender alguém da fila preferencial: L')
-        print('Adicionar à fila normal: F')
-        print('Atender alguém da fila normal: A')
-        print('Para sair: S')
-        print('Para rever os comandos: comandos')
-    if comando == 'S':
+    x=0
+    print('Total fila P: ',len(fila_p))
+    print('Total fila N: ',len(fila_n))
+    comando=input('Insira um comando: ')
+    while x<len(comando):
+        if comando[x] == 'P':
+            últimoP+=1
+            fila_p.append(últimoP)
+        elif comando[x] == 'L':
+            atendido=fila_p.pop(0)
+            print('Cliente P %d atendido' %atendido)
+        elif comando[x] == 'F':
+            últimoN+=1
+            fila_n.append(últimoN)
+        elif comando[x] == 'A':
+            atendido=fila_n.pop(0)
+            print('Cliente N %d atendido' %atendido)
+        elif comando[x] == 'C':
+            print('Para adicionar à fila preferencial digite: P')
+            print('Para atender alguém da fila preferencial digite: L')
+            print('Para adicionar à fila normal digite: F')
+            print('Para atender alguém da fila normal digite: A')
+            print('para sair digite: S')
+            print('Para rever os comandos digite C')
+        elif comando[x] == 'S':
+            s=True
+        x=x+1
+    if s == True:
         print('Programa encerrado!')
         break
-    else:
-        print('Comando inválido!')
